288f96a25fc2e8320446c8cec78fe5fee81b5012
isaac <isaac@tpirates.com>
2026-02-19 15:02:34 +0900

    feat: conf files

--- ec2-deploy/frontend/certbot.sh
+++ ec2-deploy/frontend/certbot.sh
@@ -2,6 +2,9 @@
 sudo yum update
 sudo yum install python3-certbot-dns-route53 -y
 
+sudo curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -o /etc/letsencrypt/options-ssl-nginx.conf
+sudo openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
+
 # 인증서 발급 명령어
 sudo certbot certonly --dns-route53 \
   -d tychicus-dev.icoramdeo.com \
