121433c64ec7cfed1a73a1268510d1eb8980be86
isaac <isaac@tpirates.com>
2026-02-20 12:01:33 +0900

    feat: install docker

---
+++ ec2-deploy/install_docker_compose.sh
@@ -0,0 +1,7 @@
+sudo yum install -y docker
+sudo usermod -aG docker ec2-user
+sudo systemctl enable --now docker
+
+sudo mkdir -p /usr/local/lib/docker/cli-plugins/
+sudo curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m)" -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
