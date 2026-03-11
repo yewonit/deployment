2a83bc6e3a9c27ea38399067ffd43b5ef1185054
isaac <isaac@tpirates.com>
2026-03-11 17:03:18 +0900

    fix: docker compose

--- ec2-deploy/frontend/certbot.sh
+++ ec2-deploy/frontend/certbot.sh
@@ -7,8 +7,10 @@ sudo yum install -y certbot
 sudo curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -o /etc/letsencrypt/options-ssl-nginx.conf
 sudo openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
 
-# 인증서 발급 명령어
+# 인증서 발급/확장 명령어 (--expand: 기존 인증서에 새 도메인 추가)
 sudo certbot certonly --dns-route53 \
+  --cert-name tychicus-dev.icoramdeo.com \
+  --expand \
   -d tychicus-dev.icoramdeo.com \
   -d tychicus.icoramdeo.com \
   -d tychicus-dashboard-dev.icoramdeo.com \
