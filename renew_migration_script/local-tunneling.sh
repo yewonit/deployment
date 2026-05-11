9f7f7afb3fa2b217986c90583bba4a07dc341183
isaac <isaac@tpirates.com>
2026-05-11 17:27:35 +0900

    fix: ym resource

--- renew_migration_script/local-tunneling.sh
+++ renew_migration_script/local-tunneling.sh
@@ -1,2 +1,2 @@
 echo "tunneling..."
-ssh -i ./isaac-keypair.pem -N -L 3306:localhost:3306 ec2-user@43.202.167.45
+ssh -i ./isaac-keypair.pem -N -L 3306:coramdeo-database.cpkaaeym4mdn.ap-northeast-2.rds.amazonaws.com:3306 ec2-user@43.202.167.45
